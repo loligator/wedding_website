7a54715229b68add65eb8b9c15b9083c964fd35d
Connor McDermott <connormcderm@protonmail.com>
2022-04-24 01:28:30 -0500

    Hero basic format and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
+import MainHero from './Components/Hero/hero';
+import PageStyles from './Pages/page.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={PageStyles.mainColumn}>
+      <MainHero/>
       <Link to="/rsvp">RSVP</Link>
     </div>
   );
